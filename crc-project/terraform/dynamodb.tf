3ce6eba93a347e7eea0fe27ebfffa14b7837f16b
Jericho Leal <199502689+jericholeal@users.noreply.github.com>
2025-06-18 19:06:38 -0400

    fix: adjust tab spaces

--- crc-project/terraform/dynamodb.tf
+++ crc-project/terraform/dynamodb.tf
@@ -1,13 +1,13 @@
 resource "aws_dynamodb_table" "visitor_count" {
-    name         = "crcVisitorCounter"
-    billing_mode = "PAY_PER_REQUEST"
-    hash_key     = "visitor_id"
-    attribute {
-        name = "visitor_id"
-        type = "S"
-    }
-    tags = {
-    Name        = "VisitorCountTable"
-    Environment = "Production"
-    }
+  name         = "crcVisitorCounter"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "visitor_id"
+  attribute {
+    name = "visitor_id"
+    type = "S"
+  }
+  tags = {
+	  Name        = "VisitorCountTable"
+  	Environment = "Production"
+	}
 }
