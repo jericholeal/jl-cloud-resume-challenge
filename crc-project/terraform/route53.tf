e3c620b020ecd36882b64df670689c92ff5d6803
Jericho Leal <199502689+jericholeal@users.noreply.github.com>
2025-06-18 19:05:13 -0400

    fix: typo fix

---
+++ crc-project/terraform/route53.tf
@@ -0,0 +1,27 @@
+# Hosted zone
+data "aws_route53_zone" "jericho_crc_site_zone" {
+    zone_id = var.hosted_zone_id
+    private_zone = false
+}
+
+# Alias record pointing to CloudFront distribution
+resource "aws_route53_record" "jericho_crc_site_cdn_alias" {
+    zone_id = data.aws_route53_zone.jericho_crc_site_zone.zone_id
+    name = "jericho-crc-site.xyz"
+    type = A
+
+    alias {
+        name = aws_cloudfront_distribution.jericho_crc_site_cdn_alias.domain_name
+        zone_id = "Z2FDTNDATAQYW2"
+        evaluate_target_health = false
+    }
+}
+
+# www subdomain alias record redirecting to root domain
+resource "aws_route53_record" "www_alias" {
+    zone_id = data.aws_route53_zone.primary.zone_id
+    name = "jericho-crc-site.xyz"
+    type = "CNAME"
+    ttl = 300
+    records = ["jericho-crc-site.xyz"]
+}
