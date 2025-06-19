75f5486b862a2a4b61e8c243161b0b5834906a5a
Jericho Leal <199502689+jericholeal@users.noreply.github.com>
2025-06-19 13:31:29 -0400

    fix: add quotes to type, add www to www_alias name

--- crc-project/terraform/route53.tf
+++ crc-project/terraform/route53.tf
@@ -8,7 +8,7 @@ data "aws_route53_zone" "jericho_crc_site_zone" {
 resource "aws_route53_record" "jericho_crc_site_cdn_alias" {
     zone_id = data.aws_route53_zone.jericho_crc_site_zone.zone_id
     name = "jericho-crc-site.xyz"
-    type = A
+    type = "A"
 
     alias {
         name = aws_cloudfront_distribution.jericho_crc_site_cdn_alias.domain_name
@@ -19,8 +19,8 @@ resource "aws_route53_record" "jericho_crc_site_cdn_alias" {
 
 # www subdomain alias record redirecting to root domain
 resource "aws_route53_record" "www_alias" {
-    zone_id = data.aws_route53_zone.primary.zone_id
-    name = "jericho-crc-site.xyz"
+    zone_id = data.aws_route53_zone.jericho_crc_site_zone.zone_id
+    name = "www.jericho-crc-site.xyz"
     type = "CNAME"
     ttl = 300
     records = ["jericho-crc-site.xyz"]
