a56f321e54b1c452475a5f1164731ea70b9cba9f
Jericho Leal <199502689+jericholeal@users.noreply.github.com>
2025-06-23 17:52:44 -0400

    refactor: move variable values to tfvars

--- crc-project/terraform/variables.tf
+++ crc-project/terraform/variables.tf
@@ -1,11 +1,43 @@
 variable "region" {
   description = "AWS region to deploy resources"
-  type = string
-  default = "us-east-1"
+  type = string  
 }
 
 variable "hosted_zone_id" {
   description = "Route53 Hosted Zone ID"
   type = string
-  default = "Z055437911O5KKG4KNBKR"
+}
+
+variable "lambda_s3_bucket" {
+  description = "S3 bucket for Lambda function code"
+  type = string
+}
+
+variable "lambda_s3_key" {
+  description = "S3 key for Lambda function code"
+  type = string
+}
+
+variable "lambda_function_name" {
+  description = "Name of the Lambda function for visitor counter"
+  type = string
+}
+
+variable "dynamodb_table_name" {
+  description = "DynamoDB table name for visitor counter"
+  type = string
+}
+
+variable "dynamodb_table_partition_key" {
+  description = "DynamoDB table partition key"
+  type = string
+}
+
+variable "dynamodb_table_partition_value" {
+  description = "DynamoDB table partition key value"
+  type = string
+}
+variable "dynamodb_counter_attribute" {
+  description = "DynamoDB attribute for visitor count"
+  type = string
 }
