9c669621efcd690864861da36dc77b705274d670
Jericho Leal <199502689+jericholeal@users.noreply.github.com>
2025-06-23 16:49:37 -0400

    feat: add locals.tf

---
+++ crc-project/terraform/locals.tf
@@ -0,0 +1,16 @@
+data "aws_caller_identity" "current" {}
+
+locals {
+	region = "us-east-1"
+  account_id = data.aws_caller_identity.current.account_id
+  cert_id = "4e11a0e7-3819-499c-abb3-9c39e1010ff3"
+  
+	acm_certificate_arn = "arn:aws:acm:${local.region}:{local.account_id}:certificate/${local.cert_id}"
+	dynamodb_table_arn = "arn:aws:dynamodb:${local.region}:${local.account_id}:table/${local.dynamodb_table_name}"
+	lambda_execution_logs_arn = "arn:aws:logs:${local.region}:${local.account_id}:*"
+	acm_certificate_validation_arn = local.acm_certificate_arn
+	
+	hosted_zone_id = "Z055437911O5KKG4KNBKR"
+	cd_domain_name = "dkur9puokbtno.cloudfront.net"
+	dynamodb_table_name = "crcVisitorCounter"
+}
