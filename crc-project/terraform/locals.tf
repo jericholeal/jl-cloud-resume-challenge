f853de5ebbab21537214ec34d22281b4b06ed2ec
Jericho Leal <199502689+jericholeal@users.noreply.github.com>
2025-06-23 17:16:18 -0400

    refactor: add cloudfront distribution id to locals.

--- crc-project/terraform/locals.tf
+++ crc-project/terraform/locals.tf
@@ -2,7 +2,7 @@ data "aws_caller_identity" "current" {}
 
 locals {
 	region = var.region
-  account_id = data.aws_caller_identity.current.account_id
+ 	account_id = data.aws_caller_identity.current.account_id
   cert_id = "4e11a0e7-3819-499c-abb3-9c39e1010ff3"
   
 	acm_certificate_arn = "arn:aws:acm:${local.region}:{local.account_id}:certificate/${local.cert_id}"
@@ -13,4 +13,5 @@ locals {
 	hosted_zone_id = var.hosted_zone_id
 	cd_domain_name = "dkur9puokbtno.cloudfront.net"
 	dynamodb_table_name = "crcVisitorCounter"
+	cloudfront_distribution_id = "EBXWBK6B4YJFN"
 }
