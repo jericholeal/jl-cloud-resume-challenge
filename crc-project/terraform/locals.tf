d4cc3d818d391f59fa54f1a2878a78fb2d1b9de9
Jericho Leal <199502689+jericholeal@users.noreply.github.com>
2025-06-23 21:19:45 -0400

    fix: fix acm certificate arn, add $

--- crc-project/terraform/locals.tf
+++ crc-project/terraform/locals.tf
@@ -5,7 +5,7 @@ locals {
   account_id = data.aws_caller_identity.current.account_id
   cert_id    = "4e11a0e7-3819-499c-abb3-9c39e1010ff3"
 
-  acm_certificate_arn            = "arn:aws:acm:${local.region}:{local.account_id}:certificate/${local.cert_id}"
+  acm_certificate_arn            = "arn:aws:acm:${local.region}:${local.account_id}:certificate/${local.cert_id}"
   dynamodb_table_arn             = "arn:aws:dynamodb:${local.region}:${local.account_id}:table/${local.dynamodb_table_name}"
   lambda_execution_logs_arn      = "arn:aws:logs:${local.region}:${local.account_id}:*"
   acm_certificate_validation_arn = local.acm_certificate_arn
